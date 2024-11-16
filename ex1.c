554f7ea76aa593e5a1a0eba175221320bf152ae9
noaamfarber <noaam.farber@gmail.com>
2024-11-16 11:56:32 +0200

    Update ex1.c

--- ex1.c
+++ ex1.c
@@ -14,11 +14,14 @@ int main() {
   /*Scan two integers (representing number and a position)
   Print the bit in this position. */
   int position,number;
-  scanf("%d" , &position);
+  printf("Please enter a number:\n");
   scanf("%d",&number);
+  printf("Please enter a position:\n");
+  scanf("%d" , &position);
+  
   int bit;
   bit = (number >> position) & 1;
-  printf("the bit in the position is %d: %d\n", position, bit);
+  printf("the bit in position %d of the number %d is : %d\n", position,number, bit);
 
   
   // Set bit
@@ -28,25 +31,31 @@ int main() {
   Print the output
   Now make sure it's "off" (equal to 0)
   Print the output */
-  scanf("%d", &position);
+    printf("Please enter a number:\n");
   scanf("%d", &number);
+  printf("Please enter a position:\n");
+
+  scanf("%d", &position);
   int onBit;
   onBit=number | (1 << position);
-  printf("the output when on is %d: %d\n", position, onBit);
+  printf("Number with bit %d set to 1: %d\n", position, onBit);
   int offBit;
   offBit=number & ~(1 << position);
-  printf("the output when off is %d: %d\n", position, offBit);
+  printf("Number with bit %d set to 0: %d\n", position, offBit);
 
   // Toggle bit
   printf("\nToggle bit:\n");
   /*Scan two integers (representing number and a position)
   Toggle the bit in this position
   Print the new number */
-  scanf("%d", &position);
+      printf("Please enter a number:\n");
   scanf("%d", &number);
+  printf("Please enter a position:\n");
+
+  scanf("%d", &position);
   int tog;
   tog=number ^ (1 << position);
-  printf("after toggling bit its %d: %d\n",position,tog);
+  printf("Number with bit %d toggled: %d\n",position,tog);
   
 
   
@@ -54,6 +63,8 @@ int main() {
   printf("\nEven - Odd:\n");
   /* Scan an integer
   If the number is even - print 1, else - print 0. */
+        printf("Please enter a number:\n");
+
   scanf("%d", &number);
   int isEven;
   isEven= !(number&1);
@@ -67,12 +78,17 @@ int main() {
   sum them up and print the result in hexadecimal base
   Print only 4 bits, in positions: 3,5,7,11 in the result. */
   int oct1,oct2,sum,extract,final;
+          printf("Please enter the first number (octal):\n");
+
   scanf("%o,oct1");
+            printf("Please enter the second number (octal):\n");
+
   scanf("%o,oct2");
   sum =oct1+oct2;
+  printf("The sum in hexadecimal: %X\n",&sum);
   extract = (1 << 3) | (1 << 5) | (1 << 7) | (1 << 11);
   final = sum & extract;
-  printf("final result: %X\n", final);
+  printf("The 3,5,7,11 bits are: %X\n", final);
 
 
   printf("Bye!\n");
